b469c08b9dd0fe6fc518985ec7603f1014511f5a
Valeriy Loshchenkov <klarkisek@gmail.com>
2023-10-16 18:20:00 +0530

    welcome page

--- port-acc/src/app/page.tsx
+++ port-acc/src/app/page.tsx
@@ -1,12 +1,94 @@
-import { Button, Link, Stack } from '@mui/material'
+'use client'
+import {
+	Box,
+	Button,
+	Grid,
+	Input,
+	Link,
+	Stack,
+	TextField,
+	Typography,
+} from '@mui/material'
+
+import { makeStyles } from '@mui/styles'
+
 import { FcGoogle } from 'react-icons/fc'
 import Image from 'next/image'
 
+const useStyles = makeStyles({
+	root: {
+		height: '100vh',
+	},
+	leftSection: {
+		backgroundColor: '#202b36',
+		padding: '30px',
+	},
+	logo: {
+		color: 'white',
+		fontSize: '2rem',
+	},
+	rightSection: {
+		backgroundColor: 'white',
+		display: 'flex',
+		justifyContent: 'center',
+		alignItems: 'center',
+		padding: '2rem',
+	},
+	input: {
+		width: '80%',
+		padding: '0.5rem',
+		border: '1px solid black',
+		borderRadius: '4px',
+	},
+})
+
 export default function Home() {
+	const classes = useStyles()
+
 	return (
-		<>
-			<Image src={'assets/logo.svg'} width={130} height={25} alt='papaya' />
-			<Stack direction='row' spacing={2} mt={3} justifyContent='center'>
+		<Grid container className={classes.root}>
+			<Grid item xs={8} className={classes.leftSection}>
+				<Image src={'assets/logo.svg'} width={130} height={25} alt='papaya' />
+				<Box
+					sx={{
+						color: 'white',
+						fontWeight: 700,
+						fontSize: '85px',
+						textAlign: 'center',
+						margin: '0 auto',
+						marginTop: '30%',
+					}}
+				>
+					<Typography sx={{ fontWeight: 700, fontSize: '85px' }}>
+						Create Crypto wallet
+					</Typography>
+					<Typography
+						sx={{
+							fontWeight: 700,
+							fontSize: '85px',
+							color: '#FE1C64',
+							lineHeight: 0.7,
+						}}
+					>
+						in one click&nbsp;
+						<span style={{ fontSize: '40px', color: 'white' }}>
+							And use all the
+						</span>
+					</Typography>
+					<Typography
+						sx={{
+							fontWeight: 700,
+							fontSize: '40px',
+							textAlign: 'right',
+							marginRight: '300px',
+						}}
+					>
+						benefits of web3.
+					</Typography>
+				</Box>
+			</Grid>
+
+			<Grid item xs={4} className={classes.rightSection}>
 				<Link href='https://dev.papaya.ws/api/auth/google' target='_blank'>
 					<Button
 						variant='contained'
@@ -15,12 +97,19 @@ export default function Home() {
 							backgroundColor: 'black',
 							color: 'white',
 							textTransform: 'none',
+							height: '50px',
+							width: '350px',
+							borderRadius: '8px',
+							textAlign: 'center',
+							display: 'flex',
+							justifyContent: 'center',
+							alignItems: 'center',
 						}}
 					>
 						Created with Google
 					</Button>
 				</Link>
-			</Stack>
-		</>
+			</Grid>
+		</Grid>
 	)
 }
